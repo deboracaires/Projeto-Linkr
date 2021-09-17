0a6de6f65488e3fc6ea1c3ad39571fa4e90b8e8c
Francisca <fraan928@gmail.com>
2021-09-17 04:40:31 -0300

    Conseguindo fazer um post, layout em versão inicial

--- src/components/MyPosts/Post.js
+++ src/components/MyPosts/Post.js
@@ -4,31 +4,39 @@ export default function Post({ post }) {
     let text = "likes"
     const {user, likes } = post
 
+    console.log(post)
+
     // if(likes.length === 1) {
     //     text = "like"
     // }
 
     return (
-        <PostStyle>
-            <UserInfo>
-                <img src={user.avatar} />
-                <Like />
-                <h6>{likes.length} {text}</h6>
-            </UserInfo>
-            <PostInfo>
-                <Description>
-                    <h3>{user.username}</h3>
-                    <h4>{post.text}</h4>
-                </Description>
-                <Links onClick={() => window.open(`${post.link}`,"_blank")}>
-                    <DescriptionLink>
-                        <h3>{post.linkTitle}</h3>
-                        <h4>{post.linkDescription}</h4>
-                        <h4>{post.link}</h4>
-                    </DescriptionLink>
-                    <ImageLink src={post.linkImage}></ImageLink>
-                </Links>
-            </PostInfo>
+        <PostStyle type="post">
+            {(user && likes) ?
+                <>
+                <UserInfo>
+                    <img src={user.avatar} alt="" />
+                    <Like />
+                    <h6>{likes.length} {text}</h6>
+                </UserInfo>
+                <PostInfo>
+                    <Description>
+                        <h3>{user.username}</h3>
+                        <h4>{post.text}</h4>
+                    </Description>
+                    <Links onClick={() => window.open(`${post.link}`,"_blank")}>
+                        <DescriptionLink>
+                            <h3>{post.linkTitle}</h3>
+                            <h4>{post.linkDescription}</h4>
+                            <h4>{post.link}</h4>
+                        </DescriptionLink>
+                        <ImageLink src={post.linkImage} alt="" ></ImageLink>
+                    </Links>
+                </PostInfo>
+                </>
+                : ""
+            }
+            
         </PostStyle>
     );
 }

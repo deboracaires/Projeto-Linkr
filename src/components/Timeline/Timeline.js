20d26a02a2e18e610c8f9acbfdae8aafe8554734
Francisca <fraan928@gmail.com>
2021-09-21 21:17:02 -0300

    Ajustes

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -40,8 +40,6 @@ export default function Timeline(){
         <Header/>
         <ContainerTimeline>
             
-            <Header />
-            
             <Esquerda>
                 <Titulo>timeline</Titulo>
                 <NewPost>

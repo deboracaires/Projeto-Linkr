97eccf50477f1247df6d630f4db0d255f2f84b95
joaohdallago <joaohdallago@gmail.com>
2021-09-24 00:34:05 -0300

    último mesmo kkkk

--- src/components/Header/SearchBar.js
+++ src/components/Header/SearchBar.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import {DebounceInput} from 'react-debounce-input';
 import { useState } from "react";
+import { GoSearch } from 'react-icons/go'
 
 export default function SearchBar() {
     const [searchValue, setSearchValue] = useState('')
@@ -9,14 +10,42 @@ export default function SearchBar() {
 
     return (
         <ContainerSearch>
+            <div>
             <DebounceInput
                 minLength={3}
                 debounceTimeout={300}
-                onChange={event => setSearchValue(event.target.value)} />
+                onChange={event => setSearchValue(event.target.value)}
+                placeholder='Search for people and friends' />
+                
+            <GoSearch color='#C6C6C6' size='25px' />
+            </div>
         </ContainerSearch>
     )
 }
 
 const ContainerSearch = styled.div`
+    div {
+        width: 563px;
+        height: 45px;
 
+        padding: 10px;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+
+        background-color: white;
+
+        input {
+            width: 100%;
+            height: 100%;
+            outline: none;
+
+            ::placeholder {
+                font-family: Lato;
+                color: #C6C6C6;
+            }
+        }
+    }
 `

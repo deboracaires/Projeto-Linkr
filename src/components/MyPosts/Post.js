05a810a3a4bdcb6ef758935f7906a75d8d94b9ce
Francisca <fraan928@gmail.com>
2021-09-25 05:16:00 -0300

    Mudando estado do botão like corretamente

--- src/components/MyPosts/Post.js
+++ src/components/MyPosts/Post.js
@@ -1,42 +1,58 @@
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import ReactTooltip from "react-tooltip";
-import { postDislike, postLike } from "../../service/linkr";
+import { getLikes, postDislike, postLike } from "../../service/linkr";
 import { Like, EsquerdaPost, ContainerPost, DireitaPost, ContainerLink, Liked } from "../../themes/PostsStyle";
 import ReactHashtag from "react-hashtag";
-
-export default function Post({ post, token }) {
+import { LoginValidation } from "../../login";
+/*eslint-disable*/
+export default function Post({post}) {
+    const userData = LoginValidation()
+    const { user, token } = userData;
+    
     const history = useHistory()
-
-    let { user, likes } = post
+    console.log(user)
+    let { likes } = post
     const [list, setList] = useState([])
-    const [like, setLike] = useState(false)
+    let [like, setLike] = useState(0)
     const [quantLikes, setQuantLike] = useState(0)
-
+    const [likesInPost, setLikesInPost] = useState(likes)
+    console.log(post.likes)
     let text = "likes";
 
+    useEffect(() => {
+        setQuantLike(0)
+        // if (likes.find((usr) => usr.userId === user.id )) {
+        //     setLike(true)
+        // }
+
+        // getLikes(post.user.id, token).then((res) => console.log(res.data))
+
+        setQuantLike(likes.length)
+
+        if(post && likesInPost !== [] && (likesInPost.find((us) => us.userId === user.id))) {
+            setLike(1)
+        }
+
+        setLike((likes.find((us) => us.userId == user.id)) ?
+        like = 1 : like = 0)
+    }, [])
+
     function likePost() {
-        if(like === false) {
-            setLike(true)
-            postLike(post.id, token).then((res) => console.log(res.data)).catch((err) => console.error)
+        if(like === 0) {
+            setLike(1)
             setQuantLike(quantLikes + 1)
+            postLike(post.id, token).then((res) => setLikesInPost(res.data)).catch((err) => console.error)
         }
     }
 
     function dislikePost() {
-        if(like === true) {
-            setLike(false);
-            postDislike(post.id, token).then((res) => console.log(res.data));
+        if(like === 1) {
+            setLike(0);
+            postDislike(post.id, token).then((res) => setLikesInPost(res.data)).catch((err) => console.error);
             setQuantLike(quantLikes - 1)
         }
     }
-    useEffect(() => {
-
-        if (likes.find((usr) => usr.user === user.username )) {
-            setList("list")
-        }
-        setQuantLike(likes.length)
-    }, [ user, likes ])
     
     function redirecionar(){
         history.push(`/user/${post.user.id}`)
@@ -48,33 +64,34 @@ export default function Post({ post, token }) {
     }
 
     
+console.log(like)
     return (
         <ContainerPost>
-            {((user && likes)) ?
+            {((user)) ?
                 <>
-                <EsquerdaPost>
-                    <img src={user.avatar} alt="" />
-                    {like ?
-                        <Liked onClick={dislikePost} />
-                        : <Like onClick={likePost} />
-                    }
-                    <ReactTooltip />
-                    <h3 data-tip={list}>{quantLikes} {text}</h3>
-                </EsquerdaPost>
-                <DireitaPost>
-                    <h4 onClick={redirecionar}>{post.user.username}</h4>
-                    <h5>
-                        <ReactHashtag onHashtagClick={val => directToHashtag(val)}>
-                            {post.text}
-                        </ReactHashtag>
-                    </h5>
-                    <ContainerLink onClick={() => window.open(`${post.link}`,"_blank")}>
-                        <h4>{post.linkTitle}</h4>
-                        <h5>{post.linkDescription}</h5>
-                        <a href={post.link}>{post.link}</a>
-                        <img src={post.linkImage} alt="" />
-                    </ContainerLink>
-                </DireitaPost>
+                    <EsquerdaPost>
+                        <img src={post.user.avatar} alt="" />
+                        {(like === 1) ?
+                            <Liked onClick={() => dislikePost()} />
+                            : <Like onClick={() => likePost()} />
+                        }
+                        <ReactTooltip />
+                        <h3 data-tip={3}>{quantLikes} {text}</h3>
+                    </EsquerdaPost>
+                    <DireitaPost>
+                        <h4 onClick={redirecionar}>{post.user.username}</h4>
+                        <h5>
+                            <ReactHashtag onHashtagClick={val => directToHashtag(val)}>
+                                {post.text}
+                            </ReactHashtag>
+                        </h5>
+                        <ContainerLink onClick={() => window.open(`${post.link}`,"_blank")}>
+                            <h4>{post.linkTitle}</h4>
+                            <h5>{post.linkDescription}</h5>
+                            <a href={post.link}>{post.link}</a>
+                            <img src={post.linkImage} alt="" />
+                        </ContainerLink>
+                    </DireitaPost>
                 </>
                 : ""
             }
